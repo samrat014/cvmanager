e34bb5486b682686ad2f0df24afd2053faef2a49
samrat014 <tarmaskarki014@gmail.com>
2023-05-09 11:06:20 +0545

    optimized code

--- app/Http/Controllers/admin/CVstatusController.php
+++ app/Http/Controllers/admin/CVstatusController.php
@@ -13,10 +13,6 @@ class CVstatusController extends Controller
     public function store(Request $request)
     {
 
-//        $request->validate([
-//            'task' => 'required',
-//        ]);
-
         if( $request->file('task'))
         {
             $task = $request->file('task');
@@ -27,48 +23,50 @@ class CVstatusController extends Controller
             $taskname = '';
         }
 
-    // let's check if db already have the user column
-        $checkuser = CVstatus::where('usercv_id', $request->id)->exists();
+        //upsert - making optimized code
+        CVstatus::updateOrCreate(
+            ['usercv_id' => $request->id],
+            [
+            'status' => $request->status,
+            'interview_date' => $request->interview_date,
+            'task' => $taskname,
+            ]
+        );
 
-      if(!$checkuser){
-        $user = new CVstatus();
-        $user->usercv_id = $request->id;
-        $user->status = $request->status;
-        $user->interview_date = $request->interview_date;
-        $user->task = $taskname;
-
-        $user->save();
-        }
-
-//        $userupdate = CVstatus::find('usercv_id', $request->id)->first();
+//    // let's check if db already have the user column
+//        $checkuser = CVstatus::where('usercv_id', $request->id)->exists();
 //
-//        $userupdate->status = $request->status;
-//        $userupdate->interview_date = $request->interview_date;
-//        $userupdate->task = $taskname;
+//      if(!$checkuser){
+//        $user = new CVstatus();
+//        $user->usercv_id = $request->id;
+//        $user->status = $request->status;
+//        $user->interview_date = $request->interview_date;
+//        $user->task = $taskname;
 //
-//        $userupdate->save();
-//        dd('updated');
-
-    $userupdate = CVstatus::where('usercv_id', $request->id)->first();
-        $userupdate->update([
-              'status' => $request->status,
-                'interview_date' => $request->interview_date,
-                'task' => $taskname
-            ]);
+//        $user->save();
+//        }
+//
+//        if user exists update the model
+//    $userupdate = CVstatus::where('usercv_id', $request->id)->first();
+//        $userupdate->update([
+//              'status' => $request->status,
+//                'interview_date' => $request->interview_date,
+//                'task' => $taskname
+//            ]);
 
         $user= UserCV::where('id', $request->id)->first();
         $usercv =CVstatus::where('usercv_id', $request->id)->first();
 
-        //mail when hired
-//        if($usercv->status == 'Hired'){
-//
-//            $detials = [
-//                'user' => $user->name,
-//                'technology'=> $usercv->technology,
-//            ];
-//
-//            Mail::to($user->email)->send(new \App\Mail\hiredMail($detials));
-//        }
+//        mail when hired
+        if($usercv->status == 'Hired'){
+
+            $detials = [
+                'user' => $user->name,
+                'technology'=> $usercv->technology,
+            ];
+
+            Mail::to($user->email)->send(new \App\Mail\hiredMail($detials));
+        }
 
 //        mail on the admin updates the details
         $detials = [
